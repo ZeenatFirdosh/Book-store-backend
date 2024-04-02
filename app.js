1021d8a3ed4e066ff56a25dff071c30199c98c29
Zeenat <zeenatzfq052@gmail.com>
2024-04-02 12:14:52 +0530

    backend

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://zeenatfirdoshquadri:zeenatfirdoshquadri@bookstore.kg5x7rm.mongodb.net/?retryWrites=true&w=majority&appName=Bookstore"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
